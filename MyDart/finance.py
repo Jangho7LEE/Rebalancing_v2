7cfc303bd8700c2780ff0ff3d1d3ef8a6e20cdf6
jangho.lee <uvego111@gmail.com>
2024-03-27 23:00:28 +0900

    [3/27] add

--- MyDart/finance.py
+++ MyDart/finance.py
@@ -1,5 +1,32 @@
 from MyDart.lib.utils import check_required_parameter
 from MyDart.lib.utils import check_required_parameters
+import os
+def get_corp_finance(self,corp_code_list, reset = 0):
+    '''
+    get_corp_finance는 list에 존재하는 기업의 재무재표를 가져와 stock_dic에 저장한다.
+    input: 
+        reset: 0) data/finance에 있는 기업은 가져오지 않는다
+               1) 모든 기업의 재무재표를가져온다
+    '''
+    if reset == 0:
+        saved_corp_code_list = os.listdir(self.base_path + "/finance")
+        target_list = [s for s in corp_code_list if s not in saved_corp_code_list]
+        self.save_corp_finance(target_list)
+    elif reset == 1:
+        self.save_corp_finance(corp_code_list)
+    else:
+        raise KeyError('Invalid input param: check reset!')
+    pass
+
+def save_corp_finance(self,corp_code_list):
+    '''
+    save_corp_finance는 주어진 list에 해당하는 corp code를 가지는 기업의 재무정보를
+    base_path/finance에 저장한다.
+    '''
+    for code in corp_code_list:
+        data = self.corp_finance(corp_code=code, bsns_year=self.bsns_year, reprt_code="11011")
+        if data['status'] == "000":
+            self._save_file(path = f"/finance/{code}",data= data)
 
 def corp_code(self):
     """
--- MyQuant/lib/DataManager.py
+++ MyQuant/lib/DataManager.py
@@ -9,35 +9,35 @@ class DataManager(object):
         if os.path.isdir(base_path) :self.base_path = base_path
         else: raise ValueError("Wrong path")
         self.stock_dic = {}
-        
-        
-
+    
     def openfile(self, data_path):
         """
         openfile is for open various type of files
         """
         if not os.path.exists(data_path): raise ValueError("no such path")
-        fileType = data_path.split('.')[-1]
+        chunks = data_path.split('.')
+        if len(chunks) == 1:
+            fileType = 'json'
+        else:
+            fileType = chunks[-1]
+
         return {
             "xml": self.openXML,
+            "json": self.openJson,
         }.get(fileType, self.openXML)(data_path)
-    
-    def get_corp_code_list(self):
-        '''
-        get_corp_code_list는 stock dic에 저장된 stock의 corp_code (stock code와 다름)
-        를 list형태로 반환한다
-        '''
-        sl = []
-        for s in self.stock_dic:
-            sl.append(self.stock_dic[s].corp_code)
-        return sl
     
     def openXML(self, path):
         """
         openXML is for open XML and return ElementTree
         """
         return ET.parse(path)    
-        
+    
+    def openJson(self, path):
+        '''
+        openJson은 json file을 읽어 반환
+        '''
+        with open(path, 'r') as f:
+            return json.load(f)    
 
     def save_stock_dic(self):
         """
@@ -56,9 +56,24 @@ class DataManager(object):
         load_stock_dic은 'data_path'에 존재하는 json file를 load하여 stock_dic에 저장 
         input param: data_path = base_path + corp_code   
         """
-        with open(data_path, 'r') as f:
-            dic = json.load(f)
-        
+        dic = self.openfile(data_path=data_path)
         for s in dic:
             self.stock_dic[s] = stock(**dic[s])
     
+    def get_corp_code_list(self):
+        '''
+        get_corp_code_list는 stock dic에 저장된 stock의 corp_code (stock code와 다름)
+        를 list형태로 반환한다
+        '''
+        sl = []
+        for s in self.stock_dic:
+            sl.append(self.stock_dic[s].corp_code)
+        return sl
+    
+    # 나중에 mining.py 로 옮기기
+    def read_finance(self,data_path):
+        '''
+        read_finance는 path에 명시된 json file을 읽어온다
+        '''
+        dic = self.openfile(data_path=data_path)
+        pass
